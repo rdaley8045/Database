db284db22029faf1920361d78672e55ad550cc77
Raymond Daley <70954653+rdaley8045@users.noreply.github.com>
2022-04-05 07:03:24 -0600

    Update queues.py

--- api/routers/queues.py
+++ api/routers/queues.py
@@ -1,6 +1,16 @@
 from typing import List
 from fastapi import APIRouter
-from .model import Login
+
+from .model import User, Tranactions, Query, Coin, Market, Record,Transaction
 from ..src import data
 
 router = APIRouter()
+
+@router.get("/login/{name}", name="Verify User", response_model=User)
+async def GetUsername(name:str):
+    row = data.GetUsername(name)
+    if row:
+        return User(**row[0])
+    else:
+        value = User()
+        return value
