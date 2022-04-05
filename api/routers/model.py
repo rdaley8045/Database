40ec39b8f1ddf4e4a2ea274227b3b3cc529d3055
Raymond Daley <70954653+rdaley8045@users.noreply.github.com>
2022-04-05 07:03:44 -0600

    Update model.py

--- api/routers/model.py
+++ api/routers/model.py
@@ -1,3 +1,8 @@
 from pydantic import BaseModel, validator
 from typing import Optional
-from datetime import date, datetime
+from datetime import date
+from datetime import datetime
+
+class User (BaseModel):
+    id: int = 0
+    username: str = ''
